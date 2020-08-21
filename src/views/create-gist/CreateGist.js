c9aa46dd31610e16e196c66f658cfd3189b521e6
michalfirszt <michalfiszu@gmail.com>
2020-08-21 21:33:27 +0200

    create-gist-view

---
+++ src/views/create-gist/CreateGist.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CreateGist() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-10">
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateGist;
