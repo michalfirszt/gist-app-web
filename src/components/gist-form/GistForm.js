2baf8a2f01bf50c7d7e7904f73f280e0ce16524d
michalfirszt <michalfiszu@gmail.com>
2020-08-22 15:49:16 +0200

    create-gist-loading

--- src/components/gist-form/GistForm.js
+++ src/components/gist-form/GistForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 import FileSubform from "../file-subform/FileSubform";
 
 class GistForm extends Component {
@@ -11,6 +12,7 @@ class GistForm extends Component {
             public: true,
             files: [],
             nextFileId: 1,
+            loading: false,
             client: axios.create({
                 baseURL: 'https://api.github.com/',
                 responseType: 'json',
@@ -102,6 +104,10 @@ class GistForm extends Component {
     handleSubmit(event) {
         event.preventDefault();
 
+        this.setState({
+            loading: true,
+        });
+
         let files = {};
 
         this.state.files.forEach(file => {
@@ -116,6 +122,10 @@ class GistForm extends Component {
             public: this.state.public,
             files: files,
         }).then(response => {
+            this.setState({
+                loading: false,
+            });
+
             alert('Gist created successfully');
         })
     }
@@ -139,7 +149,11 @@ class GistForm extends Component {
                         { this.selectFiles() }
                     </div>
                     <div className="form-group">
-                        <input type="submit" className="btn btn-primary"/>
+                        {this.state.loading ? (
+                            <ClipLoader loading={this.state.loading} size={100} />
+                        ) : (
+                            <input type="submit" className="btn btn-primary"/>
+                        )}
                     </div>
                 </form>
             </div>
