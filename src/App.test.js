3956974f155f909f21a3e2baa3bb8104ddcd5ce8
michalfirszt <michalfiszu@gmail.com>
2020-08-29 21:05:32 +0200

    renders-routes-test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16"
 import App from "./App";
+import Routes from "./Routes";
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -9,3 +10,8 @@ test('renders App', () => {
   const wrapper = shallow(<App />);
   expect(wrapper).toBeTruthy();
 });
+
+test('renders routes', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<Routes />)).toBeTruthy();
+})
